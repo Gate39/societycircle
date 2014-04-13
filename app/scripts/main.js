ab2c5afddc1f0b711125c5230073e55978543780
Alberto Navarro <alnavarro@Navio>
2014-04-13 19:38:46 -0400

    Adding parse credentials and source to the code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,6 +19,10 @@ require.config({
         },
         handlebars: {
             exports: 'Handlebars'
+        },
+        parse: {
+            deps: ['jquery', 'underscore'],
+            exports: 'Parse'
         }
     },
     paths: {
@@ -26,13 +30,21 @@ require.config({
         backbone: '../bower_components/backbone/backbone',
         underscore: '../bower_components/underscore/underscore',
         bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap',
-        handlebars: '../bower_components/handlebars/handlebars'
+        handlebars: '../bower_components/handlebars/handlebars',
+        parse: 'http://www.parsecdn.com/js/parse-1.2.16.min'
     }
+});
+
+require(['parse'], function(Parse) {
+    Parse.initialize("JAqTFwR1fifo4h2bHfVNheJIQUi8Fwz93XrzDInv", "xoPtDEMu96UYvKb81PQxeAaGTjOzT82XHgss27e4");
 });
 
 require([
     'backbone' , 'routes/Main'
 ], function (Backbone,Router) {
-    var router = new Router();
+    
+    new Router();
+    
     Backbone.history.start();
+    
 });
